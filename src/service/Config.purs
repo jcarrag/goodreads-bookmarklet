12e343dbc58d027a2bc9c0c26d1638f387675547
James Carragher <james@carragher.org>
2020-05-01 17:48:17 +0900

    Don't read query & emails from config

--- src/service/Config.purs
+++ src/service/Config.purs
@@ -14,9 +14,6 @@ import Prelude (bind, pure, ($), (<$>))
 newtype Config
   = Config
   { mailjetUser :: String
-  , query :: String
-  , toEmail :: String
-  , fromEmail :: String
   }
 
 configInterpreter :: Aff Config
@@ -25,14 +22,8 @@ configInterpreter =
     $ do
         _ <- loadFile
         mailjetUser <- getValue "MAILJET_USER"
-        query <- getValue "QUERY"
-        toEmail <- getValue "TO_EMAIL"
-        fromEmail <- getValue "FROM_EMAIL"
         pure $ Config
           $ { mailjetUser
-            , query
-            , toEmail
-            , fromEmail
             }
   where
   getValue :: String -> Aff String
